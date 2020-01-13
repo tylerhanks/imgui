8fb01d379d76fac19ec9663ce83593b071fd890c
omar <omarcornut@gmail.com>
2020-01-13 15:05:53 +0100

    RangeSelect/MultiScope: Transition to use FocusScope bits merged in master.
    Preserve ability to shift+arrow into an item that is part of FocusScope but doesn't carry a selection without breaking selection.

--- imgui.cpp
+++ imgui.cpp
@@ -3914,7 +3914,6 @@ void ImGui::Shutdown(ImGuiContext* context)
 
     g.PrivateClipboard.clear();
     g.InputTextState.ClearFreeMemory();
-    g.MultiSelectScopeWindow = NULL;
 
     g.SettingsWindows.clear();
     g.SettingsHandlers.clear();
@@ -8085,6 +8084,7 @@ static void ImGui::NavProcessItem(ImGuiWindow* window, const ImRect& nav_bb, con
             result->ID = id;
             result->FocusScopeId = window->DC.NavFocusScopeIdCurrent;
             result->RectRel = nav_bb_rel;
+            result->HasSelectionData = (g.NextItemData.Flags & ImGuiNextItemDataFlags_HasSelectionData) != 0;
         }
 
         // Features like PageUp/PageDown need to maintain a separate score for the visible set of items.
@@ -8098,6 +8098,7 @@ static void ImGui::NavProcessItem(ImGuiWindow* window, const ImRect& nav_bb, con
                     result->ID = id;
                     result->FocusScopeId = window->DC.NavFocusScopeIdCurrent;
                     result->RectRel = nav_bb_rel;
+                    result->HasSelectionData = (g.NextItemData.Flags & ImGuiNextItemDataFlags_HasSelectionData) != 0;
                 }
     }
 
@@ -8634,7 +8635,7 @@ static void ImGui::NavUpdateMoveResult()
         // Don't set NavJustMovedToId if just landed on the same spot (which may happen with ImGuiNavMoveFlags_AllowCurrentNavId)
         g.NavJustMovedToId = result->ID;
         g.NavJustMovedToFocusScopeId = result->FocusScopeId;
-
+        g.NavJustMovedToHasSelectionData = result->HasSelectionData;
     }
     SetNavIDWithRectRel(result->ID, g.NavLayer, result->FocusScopeId, result->RectRel);
     g.NavMoveFromClampedRefRect = false;
